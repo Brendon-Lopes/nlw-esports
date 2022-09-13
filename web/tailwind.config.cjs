359517642cac4800ef1a05ac88d102eabc9bd5c0
Brendon <brendon.lopes.21@gmail.com>
2022-09-13 04:14:23 -0300

    feat: adds background images to tailwind config file

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,15 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: 'url(./background-galaxy.png)',
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 20%, #43E7AD 60%, #E1D55D 20%)',
+      }
+    },
   },
   plugins: [],
 }
