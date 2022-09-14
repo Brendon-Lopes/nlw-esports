768ac83133e1782b8b1da8b2ab5d2541e6ceee9d
Brendon <brendon.lopes.21@gmail.com>
2022-09-14 02:20:57 -0300

    feat: adds games cards

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -15,29 +15,71 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
         </a>
 
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">DOTA 2</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
         </a>
 
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">CS GO</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
         </a>
 
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-4.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
         </a>
 
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
         </a>
 
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
         </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white block font-black">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   )
